4b5a0345010aef93b048fc4757e881a249e8383d
Ruben Werdmuller <rubenwerdmuller@gmail.com>
2024-01-24 22:29:18 +0100

    remove gitignore files

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div className="background-container">
       <div className="content-container">
-        <p>RUBEN WERDMULLER</p>
+        <p>Ruben Werdmuller</p>
         <div className="svg-container">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100%" height="100%">
             <circle cx="50%" cy="50%" r="50%" fill="#032EFF" />
